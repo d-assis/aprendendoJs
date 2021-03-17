7b08c780feef1c4e93e5384cfa0ef125a6b84b36
Gabriel de Assis Maria Pedro <gabriel.pedro@ftd.com.br>
2021-03-16 21:28:16 -0300

    ajustando último exercício

--- aula 12ex/ex014/modelo.js
+++ aula 12ex/ex014/modelo.js
@@ -1,19 +1,21 @@
-function carregar(){
-    var msg = document.querySelector('#msg')
-    var img = document.querySelector('#imagem')
-    var data = new.Date()
-    var hora = data.getHours
+    var msg = window.document.querySelector('#msg')
+    var img = window.document.querySelector('#imagem')
+    var data = new Date()
+    var hora = data.getHours()
 
     msg.innerHTML = `Agora são ${hora} horas...`
-
+    
     if(hora >= 0 && hora < 12){
         //bom dia
+        img.src = "manha.png"
+        document.querySelector('#headL').innerHTML = 'Bom dia!'
     } else if(hora >=12 && hora < 18){
         //boa tarde
+        img.src = "tarde.png"
+        document.querySelector('#headL').innerHTML = 'Boa Tarde!'
     } else {
         // boa noite
-    }
+        img.src = "noite.png"
 
-}
-
-    
+        document.querySelector('#headL').innerHTML = 'Boa noite!'
+    }    
