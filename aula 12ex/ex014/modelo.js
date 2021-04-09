006b1cb6d465640513c7cd906cb1bc9956939a46
Gabriel de Assis Maria Pedro <gabriel.pedro@ftd.com.br>
2021-04-09 19:13:21 -0300

    subindo novo exercício proposto

--- aula 12ex/ex014/modelo.js
+++ aula 12ex/ex014/modelo.js
@@ -1,3 +1,4 @@
+function carregar (){
     var msg = window.document.querySelector('#msg')
     var img = window.document.querySelector('#imagem')
     var data = new Date()
@@ -9,13 +10,18 @@
         //bom dia
         img.src = "manha.png"
         document.querySelector('#headL').innerHTML = 'Bom dia!'
+        document.body.style.backgroundColor = '#e3c13b'
+        document.querySelector('#headL').style.color = '#3a3957'
+        document.querySelector('#foot').style.color = '#3a3957'
     } else if(hora >=12 && hora < 18){
         //boa tarde
         img.src = "tarde.png"
         document.querySelector('#headL').innerHTML = 'Boa Tarde!'
+        document.body.style.backgroundColor = '#e05164'
     } else {
         // boa noite
         img.src = "noite.png"
-
         document.querySelector('#headL').innerHTML = 'Boa noite!'
+        document.body.style.backgroundColor = "#3e2973"
     }    
+}
