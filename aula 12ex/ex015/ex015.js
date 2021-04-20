4251189d6b9a3f496fa7f1714670bd3b3c784f96
Gabriel de Assis Maria Pedro <gabriel.pedro@ftd.com.br>
2021-04-19 23:05:34 -0300

    subindo continuação do último exercício

--- aula 12ex/ex015/ex015.js
+++ aula 12ex/ex015/ex015.js
@@ -3,27 +3,50 @@ function search(){
     var ano = data.getFullYear()
     var fAno = document.querySelector('#txtano').value
     var res = document.querySelector('#res')
-    //var sexo = 
 
-    console.log(`ano atual: ${ano}`)
-    console.log(`ano inserido: ${fAno}`)
+    //console.log(`ano atual: ${ano}`)
+    //console.log(`ano inserido: ${fAno}`)
 
     if(fAno.length == 0 || fAno > ano){
         window.alert("[ERRO] Ano de nascimento inválido, digite um valor válido e verifique novamente")
     }
     else{
-        window.alert("Okay!")
-    }
 
-    /*se(sexo == "masculino"){
-        se (idade >= 1 && idade < 13){
-            colocar fotinho de criança
-        }senao se(idade >= 13 && idade < 21){
-            colocar fotinho de jovem
+        /* vinculando fsex ao objeto html radsex através do método getElementsByName, que consulta e lista todos os elementos com o nome fornecido num vetor. 
+        nesse caso, a contraparte masculina de radsex (com id 'masc') ocupa do espaço [0] do vetor, enquanto o feminino (id 'fem') ocupa o espaço [1]
+        */
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - fAno
+        var foto = document.querySelector('#foto')
+
+        
+        if(fsex[0].checked){
+            res.style.textAlign = 'center'
+            res.innerHTML = `Detectamos um homem com ${idade} anos`
+
+            if(idade >= 0 && idade < 13){
+                foto.src = 'bebeM.png'
+            } else if(idade >= 13 && idade < 25){
+                foto.src = 'jovemM.png'
+            } else if(idade >= 25 && idade < 60){
+                foto.src = 'adultoM.png'
+            } else{
+                foto.src = 'idosoM.png'
+            }
+        }
+        else{
+            res.style.textAlign = 'center'
+            res.innerHTML = `Detectamos uma mulher com ${idade} anos`
+
+            if(idade >= 0 && idade < 13){
+                foto.src = 'bebeF.png'
+            } else if(idade >= 13 && idade < 25){
+                foto.src = 'jovemF.png'
+            } else if(idade >= 25 && idade < 60){
+                foto.src = 'adultoF.png'
+            } else{
+                foto.src = 'idosoF.png'
+            }
         }
     }
-    senao{
-
-    }
-    */
 }
