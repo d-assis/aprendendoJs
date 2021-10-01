b6fe27e54062a3f5151b734961b4848247fe62dc
unknown <d.assis.arte@gmail.com>
2021-10-01 08:15:50 -0300

    subindo revisão de exercícios

--- aula 14ex/ex016_revisado/ex016R.js
+++ aula 14ex/ex016_revisado/ex016R.js
@@ -3,70 +3,36 @@ function contar(){
     let ini = document.querySelector('#txti')
     let fim = document.querySelector('#txtf') 
     let passo = document.querySelector('#txtp')
+    let res = document.querySelector('#res')
 
     //validação de preenchimento
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     }
-
-    let cont = document.querySelector('#res')
-
-    //trocando mensagem de contexto
-    cont.innerHTML = 'Contando: <br>'
-
-    //casos possíveis
-    //2. fim menor que início, contagem decrescente
-    //3. início menor que fim, contagem crescente
-
-    // contagem crescente
-    if(ini < fim){
-        // tratativa de excessão para incremento nulo
-        if(passo == 0){
-            window.alert("Não é possível contar com passo 0. Substituindo o valor por 1.")
-            passo = 1
-            console.log(`passo: ${passo}`)
+    else{
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        // passo nulo
+        if (p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
-        // algoritmo da contagem
-        let i = ini
-        while( i <= fim ){
-            let ctg = document.createElement('strong')
-            ctg.setAttribute('id',`ctg${i}`)
-            ctg.innerHTML = `${i} &#x1F449;`
-            cont.appendChild(ctg)
-            i = i + passo
+        
+        if(i < f){
+            // contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-        // finalizando contagem
-        let end = document.createElement('strong')
-        end.setAttribute('id','end')
-        end.innerHTML = '&#x1F3C1'
-        cont.appendChild(end)
-    }
-    // contagem decrescente
-    else if(ini > fim){
-        // tratativa de excessão para incremento nulo
-        if(passo == 0){
-            window.alert("Não é possível contar com passo 0. Substituindo o valor por 1.")
-            passo = 1
-            console.log(`passo: ${passo}`)
+        else{
+            // contagem decrescente
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-        // algoritmo da contagem
-        let i = ini
-        while( i >= fim ){
-            let ctg = document.createElement('strong')
-            ctg.setAttribute('id',`ctg${i}`)
-            ctg.innerHTML = `${i} &#x1F449;`
-            cont.appendChild(ctg)
-            i = i - passo
-        }
-        // finalizando contagem
-        let end = document.createElement('strong')
-        end.setAttribute('id','end')
-        end.innerHTML = '&#x1F3C1'
-        cont.appendChild(end)
-    }
-    // contagem nula
-    else if(ini == fim){
-        window.alert("Não é possível contar com início igual a fim. Tente novamente com novos valores")
-        cont.innerHTML = 'Preparando contagem... <br>'
+        res.innerHTML += `\u{1F3C1}`
     }
 }
