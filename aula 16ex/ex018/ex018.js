96e405de57ec3f9f8541b01cb5909a2ca25e78d8
unknown <d.assis.arte@gmail.com>
2021-10-11 21:48:17 -0300

    subindo continuação de exercício

--- aula 16ex/ex018/ex018.js
+++ aula 16ex/ex018/ex018.js
@@ -1,21 +1,45 @@
-let n = document.querySelector('#num').value
+// pq usar a linha abaixo não funciona??? 
+//num = document.querySelector('#num').value
+let num = document.querySelector('#num')
 let t = document.querySelector('#tab')
 let r = document.querySelector('#res')
 let v = []
 
 function coletar(){
-    if(n.length == 0){
-        window.alert('insira um valor e tente novamente')
-    }else if(Number(n) < 1 || Number(n) > 100){
+    let n = Number(num.value)
+    if(n < 1 || n > 100){
         window.alert('valor inválido, insira um número dentro da faixa especificada e tente novamente')
+        console.log(n)
     }else{
-        v.push(Number(n))
+        v.push(n)
         let item = document.createElement('option')
-        item.innerHTML = `valor ${Number(n)} adicionado`
+        item.innerHTML = `valor ${n} adicionado`
         t.appendChild(item)
+        console.log(n)
     }
 }
 
 function analisar(){
+    for(let i = 0; i < 5; i++){
+        let info = document.createElement('p')
+        info.setAttribute('id',`info${i}`)
+        switch(i){
+            case 0:
+                info.innerHTML = `Ao todo temos ${v.length} números cadastrados`
+                r.appendChild(info)
+            break;
+            
+            case 1:
+            break;
+            
+            case 2:
+            break;
+            
+            case 3:
+            break;
 
+            case 4:
+            break;
+        }
+    }
 }
