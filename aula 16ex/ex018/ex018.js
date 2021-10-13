683ecb4b4f385fd2799857ba2a1b393efdf20576
unknown <d.assis.arte@gmail.com>
2021-10-13 12:03:01 -0300

    subindo continuação de exercício de funções

--- aula 16ex/ex018/ex018.js
+++ aula 16ex/ex018/ex018.js
@@ -19,26 +19,60 @@ function coletar(){
     }
 }
 
+// soma os valores adicionados ao vetor
+function somar(){
+    
+}
+
+// calcula a média dos valores adicionados
+function media(){
+
+}
+
+// compara e retorna o maior valor do vetor
+function maior(){
+
+}
+
+// compara e retorna o menor valor do vetor
+function menor(){
+
+}
+
+// exibe analises na tela
 function analisar(){
     for(let i = 0; i < 5; i++){
         let info = document.createElement('p')
         info.setAttribute('id',`info${i}`)
         switch(i){
+            // exibir comprimento do vetor
             case 0:
                 info.innerHTML = `Ao todo temos ${v.length} números cadastrados`
                 r.appendChild(info)
             break;
             
+            // exibir maior número adicionado
             case 1:
+                info.innerHTML = `O maior valor informado foi ${v}`
+                r.appendChild(info)
             break;
             
+            // exibir menor número adicionado
             case 2:
+                info.innerHTML = `O menor valor informado foi ${v}`
+                r.appendChild(info)
             break;
             
+            // exibir soma de todos os números adicionados
             case 3:
+                info.innerHTML = `Somando todos os valores, temos ${somar()}`
+                r.appendChild(info)
             break;
 
+            // exibir média dos números
             case 4:
+                info.innerHTML = `A média dos valores informados é ${media()}`
+                r.appendChild(info)
             break;
         }
     }
