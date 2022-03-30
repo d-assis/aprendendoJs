0995f11199fcff2ea7366b5c90774d51eaf06c08
unknown <d.assis.arte@gmail.com>
2022-03-30 09:16:44 -0300

    subindo ajustes no último exercício

--- prática livre/promise async await/asyncDice4.js
+++ prática livre/promise async await/asyncDice4.js
@@ -3,35 +3,18 @@
 //"preparar o terreno" para chamadas assíncronas realizadas dentro da função mãe 
 async function rolaDados (){
     //aqui declaramos algumas váriáveis para melhor organização
-    let d = 20
-    let cd = 10
+    let d = document.querySelector('#select').value
+    let cd = Math.ceil(d * 0.65)
     let roller = `http://roll.diceapi.com/json/d${d}`
     
     //e mapeamos pontos de interesse na página html
+    let l = document.querySelector('#legenda')
     let txt = document.querySelector('#txt')
     let res = document.querySelector('#output')
-    txt.innerHTML = `classe de dificuldade: ${cd}`
     
-    /*
-    fetch(roller)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-        return data.dice[0].value
-    })
-    .then((diceRes) => {
-        if(diceRes >= cd && diceRes != d){
-            res.innerHTML = `rolou ${diceRes} e passou no teste!`
-        } else if(diceRes == 1){
-            res.innerHTML = `rolou ${diceRes}! FALHA CRÍTICA!`
-        } else if(diceRes == d){
-            res.innerHTML = `rolou ${diceRes}! ACERTO CRÍTICO!`
-        } else {
-            res.innerHTML = `rolou ${diceRes} e falhou no teste...`
-        }
-    })
-    .catch((err) => console.log(`oops! ${err}`))
-    */
+    // alterando textos em parte da página após o click do usuário
+    txt.innerHTML = `classe de dificuldade: ${cd}`
+    l.innerHTML = ""
 
     // Na notação async/await, tratamos os valores de resolve e reject de uma promise de forma diferente.
     // abrimos um bloco try{} para invocar a chamada assíncrona e realizar processos com o valor de retorno no caso de sucesso, 
